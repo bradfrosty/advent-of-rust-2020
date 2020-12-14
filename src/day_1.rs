f7961d919f4d6e7da8f8d2bffbe82419e3ca5720
Brad Frost <bfrost2893@gmail.com>
2020-12-14 10:40:27 -0500

    Day 1

---
+++ src/day_1.rs
@@ -0,0 +1,8 @@
+
+pub fn part_1() {
+    println!("Hello Part 1!");
+}
+
+pub fn part_2() {
+    println!("Hello Part 2!");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod day_1;
+
 fn main() {
     println!("Hello, world!");
+    day_1::part_1();
+    day_1::part_2();
 }
